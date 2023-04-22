f77ea245f8bb7e1bcd52a6d771aa1999125b4a5f
yuvalohana <42322357+yuvalohana@users.noreply.github.com>
2023-04-22 20:32:32 +0300

    third commit

--- Yuvalohana.java
+++ Yuvalohana.java
@@ -1,7 +1,7 @@
 public class Yuvalohana{
 	public static void main(String[] args) {
 		for (int i=1;i<=10; i++) {
-		System.out.println("Yuval Ohana ...");
+		System.out.println("Yuval Ohana ha ha ...");
 		}
 	}
 }
